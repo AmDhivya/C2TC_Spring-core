d8e79f5913a8147be7ccfed0476b3dae7fa464af
hp5cd <hp5cd@dhivya>
2025-11-04 20:53:36 +0530

    commit

--- springdemo/src/main/java/com/tnsif/springdemo/Airtel.java
+++ springdemo/src/main/java/com/tnsif/springdemo/Airtel.java
@@ -1,17 +1,3 @@
 package com.tnsif.springdemo;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App {
-    public static void main(String[] args) {
-    	
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-        Airtel airtel = (Airtel) context.getBean("air");
-
-        // Call methods of the Airtel bean
-        airtel.call();
-        airtel.data();
-    }
-}
